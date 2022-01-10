724512ab617e383f96593d779b7ab9d30b8ae8b0
Viktor Martinović <53435298+martino-vic@users.noreply.github.com>
2022-01-10 01:17:44 +0000

    corrected bibtex key to 557564

--- lexibank_streitberggothic.py
+++ lexibank_streitberggothic.py
@@ -5,15 +5,16 @@ from pylexibank import FormSpec
 
 REP = [(x, "") for x in "†*[]~?;+-"] + [(" ", "_"), (",_", ", ")]
 
+
 class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
     id = "streitberggothic"
-    
+
     form_spec = FormSpec(separators=",", first_form_only=True,
-        replacements=REP)
+                         replacements=REP)
 
     def cmd_makecldf(self, args):
-    
+
         # add bib
         args.writer.add_sources()
         args.log.info("added sources")
@@ -25,7 +26,7 @@ class Dataset(BaseDataset):
                     Name=concept["sense"]
                     )
         args.log.info("added concepts")
-        
+
         # add languages
         args.writer.add_languages()
         args.log.info("added languages")
@@ -35,10 +36,9 @@ class Dataset(BaseDataset):
                 "Streitberg-1910-3659.tsv", delimiter="\t")[1:]):
             args.writer.add_forms_from_value(
                 ID=idx,
-                Language_ID = str(0),
-                Parameter_ID = str(idx),
-                Value = row[0],
-                Source = "Streitberg1910"
+                Language_ID=str(0),
+                Parameter_ID=str(idx),
+                Value=row[0],
+                Source="557564"
                 )
         args.log.info("added forms")
-        
