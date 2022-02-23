0987561d6bd7e5972ea75c43b2f518135fa7dccb
lingulist <mattis.list@lingpy.org>
2022-02-23 11:47:41 +0100

    first example for dictionary representation

--- lexibank_streitberggothic.py
+++ lexibank_streitberggothic.py
@@ -2,6 +2,8 @@ import pathlib
 
 from pylexibank import Dataset as BaseDataset
 from pylexibank import FormSpec, Concept
+import pylexibank
+from cldfbench import CLDFSpec
 from clldutils.misc import slug
 import attr
 
@@ -22,38 +24,65 @@ class Dataset(BaseDataset):
 
     concept_class = CustomConcept
 
+    def cldf_specs(self):
+        return {
+            None: pylexibank.Dataset.cldf_specs(self),
+            "dictionary": CLDFSpec(
+                module="Dictionary",
+                dir=self.cldf_dir,
+            ),
+        }
+
     def cmd_makecldf(self, args):
 
-        # add bib
-        args.writer.add_sources()
-        args.log.info("added sources")
+        with self.cldf_writer(args) as writer:
+            writer.add_sources()
+            args.log.info("added sources")
 
-        # add concept
-        concepts = {}
-        for i, concept in enumerate(self.concepts):
-            idx = str(i+1)+"_"+slug(concept["sense"])
-            args.writer.add_concept(
-                    ID=idx,
-                    Name=concept["sense"],
-                    POS=concept["pos"],
-                    Concepticon_ID=concept["CONCEPTICON_ID"],
-                    Concepticon_Gloss=concept["CONCEPTICON_GLOSS"]
-                    )
-            concepts[concept["sense"], concept["pos"]] = idx
-        args.log.info("added concepts")
+            # add concept
+            concepts = {}
+            for i, concept in enumerate(self.concepts):
+                idx = str(i+1)+"_"+slug(concept["sense"])
+                writer.add_concept(
+                        ID=idx,
+                        Name=concept["sense"],
+                        POS=concept["pos"],
+                        Concepticon_ID=concept["CONCEPTICON_ID"],
+                        Concepticon_Gloss=concept["CONCEPTICON_GLOSS"]
+                        )
+                concepts[concept["sense"], concept["pos"]] = idx
+            args.log.info("added concepts")
 
-        # add languages
-        args.writer.add_languages()
-        args.log.info("added languages")
+            # add languages
+            writer.add_languages()
+            args.log.info("added languages")
 
-        # add forms
-        for idx, row in enumerate(self.raw_dir.read_csv(
-                "Streitberg-1910-3659.tsv", delimiter="\t", dicts=True)[1:]):
-            args.writer.add_forms_from_value(
-                Local_ID=idx,
-                Language_ID="Gothic",
-                Parameter_ID=concepts[row["sense"], row["pos"]],
-                Value=row["form"],
-                Source="557564")
+            language_table = writer.cldf["LanguageTable"]
 
-        args.log.info("added forms")
+            # add forms
+            for idx, row in enumerate(self.raw_dir.read_csv(
+                    "Streitberg-1910-3659.tsv", delimiter="\t", dicts=True)[1:]):
+                writer.add_forms_from_value(
+                    Local_ID=idx,
+                    Language_ID="Gothic",
+                    Parameter_ID=concepts[row["sense"], row["pos"]],
+                    Value=row["form"],
+                    Source="557564")
+            args.log.info("added forms")
+        with self.cldf_writer(args, cldf_spec="dictionary", clean=False) as writer:
+            writer.cldf.add_component(language_table)
+            for idx, row in enumerate(self.raw_dir.read_csv(
+                "Streitberg-1910-3659.tsv", delimiter="\t", dicts=True)):
+                entry_id = "{0}-{1}".format(idx+1, slug(row["form"]))
+                sense_id = "{0}-{1}".format(idx+1, slug(row["sense"]))
+                writer.objects["EntryTable"].append({
+                    "ID": entry_id,
+                    "Language_ID": "Gothic",
+                    "Headword": row["form"],
+                    "Part_Of_Speech": row["pos"]
+                    })
+                writer.objects["SenseTable"].append({
+                    "ID": sense_id,
+                    "Description": row["sense"],
+                    "Entry_ID": entry_id
+                    })
